1de3525ae56505a3799840f4f5cf2bb44f0e3eea
AlfHan94 <alfredhandin@gmail.com>
2026-04-09 14:44:13 +0200

    Add ProductsController, ErrorViewModel, and products view
    Implemented ProductsController to display products from a fake in-memory data store. Added ErrorViewModel for error handling. Created Index.cshtml to list products in stock with quantity and location, including UI indicators for low stock and test data notice.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IntralogisticsSimulator.Data;
+namespace IntralogisticsSimulator.Controllers;
+
+
+public class ProductsController : Controller
+{
+    public IActionResult Index()
+    {
+        var products = FakeDataStore.Products;
+        return View(products);
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace IntralogisticsSimulator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
