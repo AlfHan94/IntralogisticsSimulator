637436211b5c9421b7f5184e1e30ab2727a8c3dc
AlfHan94 <alfredhandin@gmail.com>
2026-04-14 13:25:34 +0200

    Add Orders page with controller, view, and nav links
    Implemented OrdersController and Index view to display all orders with status, creation date, and order items. Updated site navigation to include Products and Orders pages with icons. Added placeholder for future order creation feature.

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IntralogisticsSimulator.Data;
+
+
+namespace IntralogisticsSimulator.Controllers;
+
+public class OrdersController : Controller
+{
+    public IActionResult Index()
+    {
+        var orders = FakeDataStore.Orders;
+        return View(orders);
+    }
+}
